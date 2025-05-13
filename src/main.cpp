6a189cb3c9a27143c32c9a990ee2e84f844c24f4
Pythonuser9 <edisonsayya@gmail.com>
2025-05-13 06:11:10 -0700

    a
    a

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@ void disabled() {
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-int atn = 6;
+int atn = 0;
 int pressed = 0;
 int automacro = 0;
 string autstr;
